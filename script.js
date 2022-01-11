2a6f35f62629adb919eef6cdae79c89f7375a762
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-01-11 20:22:08 -0300

    Enviando o requisito 14

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.getElementById('buttonForm1').addEventListener('click', () => {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
+
   if (email === 'tryber@teste.com' && password === '123456') {
     window.alert('Olá, Tryber!');
   } else {
